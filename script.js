83f97c3e02833c8dd26fb044ce0b2fd038729b05
tarasend <tz.taras@gmail.com>
2024-02-05 14:35:43 +0200

    animation practice

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 console.log('greetings universe!');
 
-let pugout = document.querySelector('.pugarea');
-let pugNames = ['pug-1.jpg', 'pug-2.jpg', 'pug-3.jpg', 'pug-4.jpg'];
+// let pugout = document.querySelector('.pugarea');
+// let pugNames = ['pug-1.jpg', 'pug-2.jpg', 'pug-3.jpg', 'pug-4.jpg'];
 
 
-for (ver = 1; ver <= 4;ver++) {
-    let img = document.createElement('img');
+// for (ver = 1; ver <= 4;ver++) {
+//     let img = document.createElement('img');
     
-    img.src = './img/pugs/pug-' + ver + '.jpg';
-    pugout.append(img);
-}
+//     img.src = './img/pugs/pug-' + ver + '.jpg';
+//     pugout.append(img);
+// }
 
+let an1 = document.getElementById('main-pug');
+an1.addEventListener('click', () => {
+    // alert('wuf!');
+    // let a = document.querySelector('.pugarea');
+    // a.classList.toggle('animate1');
+    
+});
