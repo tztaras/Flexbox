78c1fd522a435d2361bce6d773d2590f2cf046dc
tarasend <tz.taras@gmail.com>
2024-02-06 17:58:07 +0200

    animation pratice

--- script.js
+++ script.js
@@ -16,5 +16,24 @@ an1.addEventListener('click', () => {
     // alert('wuf!');
     let a = document.querySelector('.pugarea');
     a.classList.toggle('animate1');
+
+   
     
+    // a.classList.toggle('animate1-back');
+    
+    
+});
+
+let an2 = document.querySelector('.pug1').addEventListener('click', () => {
+    let a = document.querySelector('.pug1');
+    a.classList.toggle('animate1-1');
+});
+
+let an3 = document.querySelector('.pug2').addEventListener('click', () => {
+    let a = document.querySelector('.pug2').classList.toggle('animate3');
+    // a.classList.toggle('animate3');
+});
+
+let an4 = document.querySelector('.pug3').addEventListener('click', () => {
+    let a = document.querySelector('.pug3').classList.toggle('animate4');
 });
